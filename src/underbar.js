eb57a0d68f265b6f6ef2493ad59cc70d13e8dc70
David A Blanchard <dblanchard13@users.noreply.github.com>
2014-12-11 11:54:02 -0800

    _.last & _.each
    First take on _.last & _.each

--- src/underbar.js
+++ src/underbar.js
@@ -37,6 +37,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length-1] : array.slice(n, (array.length));
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +46,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i=0; i<collection.length; i++){
+        iterator(collection[i], i, collection);
+      }
+    }
+    else{
+      for(var key in collection){
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
