ae928bb2d148f77edfae1119939c1533cc03eef1
David A Blanchard <dblanchard13@users.noreply.github.com>
2014-12-11 12:12:22 -0800

    _.filter

--- src/underbar.js
+++ src/underbar.js
@@ -77,6 +77,11 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    _.each(collection, function(item){
+      if(test(item)){
+        return item;
+      }
+    });
   };
 
   // Return all elements of an array that don't pass a truth test.
