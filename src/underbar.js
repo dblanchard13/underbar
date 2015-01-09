15a70d08a98e7dac3e35416d94e5225c45296a1a
dblanchard13 <davidb.prof@gmail.com>
2015-01-08 23:42:05 -0800

    Completed _.extend & _.defaults
    _.defaults is nearly identical to _.extend, just with a slightly more
    thorough truth check.

--- src/underbar.js
+++ src/underbar.js
@@ -248,11 +248,29 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 0; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var prop in source) {
+        if (source.hasOwnProperty(prop)) {
+            obj[prop] = source[prop];
+        }
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 0; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var prop in source) {
+        if (source.hasOwnProperty(prop) && !obj.hasOwnProperty(prop)) {
+            obj[prop] = source[prop];
+        }
+      }
+    }
+    return obj;
   };
 
 
