96f70b1178b43553a6db54842d3feff15148f9b7
dblanchard13 <davidb.prof@gmail.com>
2015-01-09 17:24:27 -0800

    Finished _.sync

--- src/underbar.js
+++ src/underbar.js
@@ -314,6 +314,7 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+   
   };
 
   // Delays a function for the given number of milliseconds, and then calls
@@ -323,6 +324,10 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    setTimeout(function(){
+      return func.apply(null, args);
+    }, wait);
   };
 
 
